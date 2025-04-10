0cb8e31ac1709c4312a7a5d626c476a44b958547
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-04-10 12:30:51 -0300

    Estilizando e direcionando nav para rotas

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,75 +1,32 @@
 <template>
   <div class="header-content">
     <img class="logo" src="../assets/upe.png" alt="upe-logo" />
-    <ul>
-      <NavOption><p>Home</p></NavOption>
-      <NavOption><p>Ensino</p></NavOption>
-      <NavOption><p>Pesquisa e Extensão</p></NavOption>
-      <NavOption><p>Contato</p></NavOption>
-    </ul>
-    <SearchBar></SearchBar>
+    <NavOption />
+    <SearchBar />
   </div>
 </template>
 
 <script>
 import NavOption from "./headerComponents/NavOption.vue";
-import SearchBar from "./headerComponents/SearchBar.vue"
+import SearchBar from "./headerComponents/SearchBar.vue";
 export default {
   components: {
     NavOption,
-    SearchBar
+    SearchBar,
   },
 };
 </script>
 
-<style scoped>
+<style>
 .header-content {
   display: flex;
   gap: 40px;
 }
-
-ul {
-  display: flex;
-  flex-direction: row;
-  gap: 60px;
-  flex-wrap: nowrap;
-}
-
-
-p {
-  color: white;
-  font-size: 25px;
-  white-space: nowrap;
-  margin: 0px;
-}
-
 
 .logo {
   width: 100px;
   height: auto;
 }
 
-@media (max-width: 1100px) {
-  ul {
-    gap: 30px;
-  }
-
-  p {
-    font-size: 20px;
-  }
-}
-
-@media (max-width: 1000px) {
-  ul {
-    display: none;
-  }
-  header {
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    justify-content: center;
-  }
-  
-}
 
 </style>
--- src/components/headerComponents/NavOption.vue
+++ src/components/headerComponents/NavOption.vue
@@ -1,21 +1,68 @@
 <template>
   <div class="nav-option">
-    <slot></slot>
+    <ul>
+      <li>
+        <router-link to="/home">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/ensino">Ensino</router-link>
+      </li>
+      <li>
+        <router-link to="/pesq-ext">Pesquisa e Extensão</router-link>
+      </li>
+      <li>
+        <router-link to="/contato">Contato</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
-<script></script>
+<script>
+</script>
 
 <style scoped>
-.nav-option {
+li {
+  list-style-type: none;
+}
+
+
+a {
+  color: white;
+  font-size: 25px;
+  text-decoration: none;
   border: 2px solid transparent;
   border-radius: 5px;
   padding: 5px;
   transition: border-color 0.3s ease;
 }
 
-.nav-option:hover {
+a:hover {
   border-color: white;
   cursor: pointer;
+}
+
+a:active,
+a.router-link-active {
+  text-decoration: underline 1px;
+  border-color: transparent;
+}
+
+ul {
+  display: flex;
+  flex-direction: row;
+  gap: 60px;
+  flex-wrap: nowrap;
+}
+
+@media (max-width: 1100px) {
+  ul {
+    gap: 30px;
+  }
+}
+
+@media (max-width: 1000px) {
+  ul {
+    display: none;
+  }
 }
 </style>
