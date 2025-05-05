3c36503c77a62369b3ae75a7515473fbfd376b54
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-05-05 16:12:55 -0300

    Mecanismo de busca

--- src/components/headerComponents/SearchBar.vue
+++ src/components/headerComponents/SearchBar.vue
@@ -1,11 +1,23 @@
 <template>
-    <div class="search-wrapper">
-      <input type="text" placeholder="Buscar" class="search" />
-    </div>
-  </template>
-  
+  <div class="search-wrapper">
+    <input type="text" placeholder="Buscar" v-model="searchQuery" class="search" @keyup.enter="goToNews" />
+  </div>
+</template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  methods: {
+    goToNews() {
+      this.$router.push({ path: "/news", query: { search: this.searchQuery } });
+    },
+  },
+};
+</script>
 
 <style scoped>
 .search-wrapper {
@@ -16,7 +28,7 @@
 
 .search {
   border-radius: 50px;
-  width: 150px; 
+  width: 150px;
   padding: 8px 30px 8px 10px;
   border: 1px solid #ccc;
   font-size: 16px;
@@ -27,5 +39,4 @@
     display: none;
   }
 }
-
 </style>
--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <h1>Notícias</h1>
     <ul v-if="hasNotice">
-      <li v-for="news in arrayOfNews" :key="news.id">
+      <li v-for="news in showNews" :key="news.id">
         <NewsComponent :news="news" />
       </li>
     </ul>
@@ -13,26 +13,30 @@
 
 <script>
 import NewsComponent from "./NewsComponent.vue";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
       totalPerPage: 5,
-      arrayOfNews: [],
-      error: null
+      showNews: [],
+      allNews: [],
+      error: null,
     };
   },
   methods: {
-    loadNews() {
+    async loadNews() {
       axios
         .get(
-          'https://vue-http-demo-2fdc2-default-rtdb.firebaseio.com/shelf.json'
+          "https://vue-http-demo-2fdc2-default-rtdb.firebaseio.com/shelf.json"
         )
         .then((response) => {
           const data = response.data;
           const results = [];
-          for (const id in data) {
+
+          const reversedKeys = Object.keys(data).reverse();
+
+          for (const id of reversedKeys) {
             results.push({
               id: id,
               title: data[id].title,
@@ -40,54 +44,74 @@ export default {
               references: data[id].references,
               overview: data[id].overview,
               published: data[id].published,
-              imgPath: data[id].imgPath
+              imgPath: data[id].imgPath,
             });
           }
-          this.arrayOfNews = results;
-          console.log(this.arrayOfNews);
+          this.allNews = results;
+          this.loadRouteQuery(this.$route);
         })
         .catch((error) => {
           console.log(error);
-          this.error = 'Failed too fetch data - please try again later';
+          this.error = "Failed too fetch data - please try again later";
         });
-    }
+    },
+    loadRouteQuery(newRoute) {
+      const query = newRoute.query?.search || "";
+
+      if (newRoute) {
+        const searchWords = query.split(" ");
+
+        this.showNews = this.allNews.filter((newsItem) => {
+          return searchWords.some((word) =>
+            newsItem.title.toLowerCase().includes(word.toLowerCase())
+          );
+        });
+      } else {
+        this.showNews = this.allNews;
+      }
+    },
   },
   components: {
-    NewsComponent
+    NewsComponent,
   },
   computed: {
     hasNotice() {
-      return this.arrayOfNews.length != 0;
-    }
+      return this.showNews.length != 0;
+    },
   },
-  mounted() {
+  created() {
     this.loadNews();
+  },
+  watch: {
+    $route(newRoute) {
+      this.loadRouteQuery(newRoute);
+    }
   }
 };
 </script>
 
 <style scoped>
-  .content {
-    border: 1px solid rgb(205, 197, 197, 0.4);
-    width: 80%;
-    padding: 20px;
-    margin-top: 20px;
-  }
-  ul {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    row-gap: 20px;
-    margin: 0;
-    padding-inline-start: 0;
-  }
-  h1 {
-    line-height: 1;
-    margin: 0 0 15px;
-    font-size: 35px;
-    color: #444444;
-    font-family: Roboto Condensed;
-    
-    unicode-bidi: isolate;
-  }
+.content {
+  border: 1px solid rgb(205, 197, 197, 0.4);
+  width: 80%;
+  padding: 20px;
+  margin-top: 20px;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  row-gap: 20px;
+  margin: 0;
+  padding-inline-start: 0;
+}
+h1 {
+  line-height: 1;
+  margin: 0 0 15px;
+  font-size: 35px;
+  color: #444444;
+  font-family: Roboto Condensed;
+
+  unicode-bidi: isolate;
+}
 </style>
