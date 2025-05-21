935a4a92aab1851a97f7945affa52295f04f2441
Gabriel Lopes <gabriel.lopes.albuquerque@gmail.com>
2025-05-21 14:49:27 -0300

    Implemetando o Icon de User

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,9 +7,15 @@
       <img class="logo" src="../assets/upe.png" alt="upe-logo" />
       <NavOption />
       <SearchBar />
+
+      <!-- Gabriel botou o icone de login -->
+      <router-link to="/login" class="login-icon" title="Login">
+        <img src="@/assets/icons/user-icon.svg" alt="Login" />
+      </router-link>
     </div>
   </div>
 </template>
+
 
 <script>
 import NavOption from "./headerComponents/NavOption.vue";
@@ -48,4 +54,28 @@ export default {
   width: 100px;
   height: auto;
 }
+
+
+.login-icon {
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+}
+
+.login-icon img {
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 2px solid white;
+  padding: 4px;
+  background-color: #262166;
+  transition: transform 0.2s ease;
+}
+
+.login-icon img:hover {
+  transform: scale(1.1);
+}
+
+
 </style>
+
